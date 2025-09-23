7c6af3f3fe675fae08b43a21638a7d939efff0f3
vaibhavyaramwar <you@example.com>
2025-09-23 11:42:55 +0530

    Created Crew Implementation for Agents,Tasks and crew Pipeline

--- crew.py
+++ crew.py
@@ -1,21 +1,14 @@
-from crewai import Agent, Crew, Process
+from crewai import Agent, Crew, Task,Process
 from crewai.project import CrewBase, agent, crew
 from crewai_tools import SerperDevTool
+from crewai.decorators import agent,task
 
 @CrewBase
-class ResumeATSCrew():
-  """Resume ATS crew"""
+class ResumeATSCrew:
+  """Crew for handling resume ATS tasks."""
 
   agents_config = "config/agents.yaml"
-
-
-from crewai import Agent
-from crewai_tools import SerperDevTool
-from crewai.decorators import agent
-
-class MyAgents:
-
-  agents_config = "config/agents.yaml"
+  tasks_config = "config/tasks.yaml"
   
   @agent
   def parser_agent(self) -> Agent:
@@ -87,4 +80,86 @@ class MyAgents:
       config=self.agents_config['technical_question_agent'],
       verbose=True,
       tools=[SerperDevTool()]
+    )
+
+  @task
+  def parse_resume_task(self) -> Task:
+    return Task(
+      config=self.tasks_config['parse_resume_task']  # type: ignore[index]
+    )
+
+  @task
+  def rewrite_for_ats_task(self) -> Task:
+    return Task(
+      config=self.tasks_config['rewrite_for_ats_task']
+    )
+
+  @task
+  def refine_bullets_task(self) -> Task:
+    return Task(
+      config=self.tasks_config['refine_bullets_task']
+    )
+
+  @task
+  def evaluate_ats_task(self) -> Task:
+    return Task(
+      config=self.tasks_config['evaluate_ats_task']
+    )
+
+  @task
+  def general_question_task(self) -> Task:
+    return Task(
+      config=self.tasks_config['general_question_task']
+    )
+
+  @task
+  def managerial_question_task(self) -> Task:
+    return Task(
+      config=self.tasks_config['managerial_question_task']
+    )
+
+  @task
+  def hr_question_task(self) -> Task:
+    return Task(
+      config=self.tasks_config['hr_question_task']
+    )
+
+  @task
+  def behavioral_question_task(self) -> Task:
+    return Task(
+      config=self.tasks_config['behavioral_question_task']
+    )
+
+  @task
+  def technical_question_task(self) -> Task:
+    return Task(
+      config=self.tasks_config['technical_question_task']
+    )
+
+  @crew
+  def full_pipeline_crew(self) -> Crew:
+    return Crew(
+      agents=[
+        self.parser_agent(),
+        self.ats_writer_agent(),
+        self.refiner_agent(),
+        self.evaluator_agent(),
+        self.general_question_agent(),
+        self.managerial_question_agent(),
+        self.hr_question_agent(),
+        self.behavioral_question_agent(),
+        self.technical_question_agent(),
+      ],
+      tasks=[
+        self.parse_resume_task(),
+        self.rewrite_for_ats_task(),
+        self.refine_bullets_task(),
+        self.evaluate_ats_task(),
+        self.general_question_task(),
+        self.managerial_question_task(),
+        self.hr_question_task(),
+        self.behavioral_question_task(),
+        self.technical_question_task(),
+      ],
+      process=Process.sequential
     )
